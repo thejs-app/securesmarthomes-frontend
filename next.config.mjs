3adccbc38dbc59db7b81e18450c719a32b7dc0c9
Bilal Yaver <bilalyaver@outlook.com>
2024-09-11 23:49:55 +0300

    Update API URLs to use dynamic base URL

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,31 @@
 /** @type {import('next').NextConfig} */
+
+let apiBaseUrl;
+
+const env = process.env.NODE_ENV;
+
+if (env == "development") {
+  apiBaseUrl = 'http://localhost:8081';
+} else {
+  apiBaseUrl = 'https://dashboard.securesmarthomes.com';
+}
+
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    API_URL: "http://localhost:8081/api/",
+    API_URL: `${apiBaseUrl}/api/`,
     API_KEY: "temporaryKey",
-    IMAGE_PATH: "http://localhost:8081/images",
+    IMAGE_PATH: `${apiBaseUrl}/images`,
   },
   images: {
     remotePatterns: [
       {
         protocol: 'http',
         hostname: 'localhost',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dashboard.securesmarthomes.com',
       },
     ],
   },
