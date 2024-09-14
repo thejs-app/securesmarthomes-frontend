481ad5487983a15a32e891f12f559aa815ddee87
Bilal Yaver <bilalyaver@outlook.com>
2024-09-15 00:45:22 +0300

    Add Google Analytics script to _app.js for tracking

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
 import "@/styles/globals.css";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <main>
+    <Script src="https://www.googletagmanager.com/gtag/js?id=GA_MEASUREMENT_ID" />
+    <Script id="google-analytics">
+      {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-EWKP48CM6E');
+        `}
+    </Script>
+    <Component {...pageProps} />
+  </main>;
 }
